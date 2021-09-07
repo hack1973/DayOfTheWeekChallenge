d5d9bbd5d80880c19dd024a93c120ec177a364aa
Michael Hacker <hack1973@swbell.net>
2021-09-07 08:55:46 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DayOfTheWeekChallenge.printDayOfTheWeek(-1);
+        DayOfTheWeekChallenge.printDayOfTheWeek(0);
+        DayOfTheWeekChallenge.printDayOfTheWeek(1);
+        DayOfTheWeekChallenge.printDayOfTheWeek(2);
+        DayOfTheWeekChallenge.printDayOfTheWeek(3);
+        DayOfTheWeekChallenge.printDayOfTheWeek(4);
+        DayOfTheWeekChallenge.printDayOfTheWeek(5);
+        DayOfTheWeekChallenge.printDayOfTheWeek(6);
+        DayOfTheWeekChallenge.printDayOfTheWeek(7);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(-1);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(0);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(1);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(2);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(3);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(4);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(5);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(6);
+        DayOfTheWeekChallenge.printDayOfTheWeekWithIfElse(7);
+    }
+}
